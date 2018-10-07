7d89865ef08785c9265dfbaa552435f8d8686dde
林凯 <kklin@bogon.dhcp>
2018-10-07 23:10:11 +0800

    chapter 7 & 8

---
+++ chapter_8/greeter.py
@@ -0,0 +1,14 @@
+def greet_user():
+    """显示简单的问候语"""
+    print("Hello!")
+
+
+greet_user()
+
+
+def greet_user(username):
+    """显示简单的问候语"""
+    print("Hello," + username.title() + "!")
+
+
+greet_user('jesse')
